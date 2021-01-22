52e5d9d89b72e5229a9b2e3f716fac90e11c071d
Lucy Wyman <wyman.lucy@gmail.com>
2021-01-22 11:10:10 -0800

    (maint) Bump maximum Puppet version to include 7.x, prep for release
    Also adds stdlib to `.fixtures.yml`, and modifies the broken tests to
    create a temporary script file to verify that running with a script
    works.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## Release 0.2.0
+
+### New features
+
+* **Bump maximum Puppet version to include 7.x** ([#14](https://github.com/puppetlabs/puppetlabs-azure_inventory/pull/14))
+
 ## Release 0.1.0
 
 This is the initial release.
--- spec/plans/init_spec.rb
+++ spec/plans/init_spec.rb
@@ -1,26 +1,19 @@
 # frozen_string_literal: true
 
 require 'json'
+require 'pathname'
 require 'spec_helper'
 require 'bolt_spec/plans'
 
 describe 'secure_env_vars' do
   include BoltSpec::Plans
 
-  let(:env_vars)    { { 'foo' => 'bar' } }
-  let(:output)      { { 'stdout' => 'success' } }
-  let(:bolt_config) { { 'modulepath' => RSpec.configuration.module_path } }
-
-  let(:command_params) { params.slice('targets', 'command') }
-  let(:script_params)  { params.slice('targets', 'script') }
-
-  let(:params) do
-    {
-      'targets' => 'localhost',
-      'command' => 'whoami',
-      'script'  => 'spec/script.sh'
-    }
-  end
+  let(:env_vars)        { { 'foo' => 'bar' } }
+  let(:output)          { { 'stdout' => 'success' } }
+  let(:bolt_config)     { { 'modulepath' => RSpec.configuration.module_path } }
+  let(:target)          { { 'targets' => 'localhost' } }
+  let(:command)         { 'whoami' }
+  let(:command_params)  { { 'command' => command }.merge(target) }
 
   before(:all) do
     BoltSpec::Plans.init
@@ -34,38 +27,43 @@ describe 'secure_env_vars' do
     ENV['BOLT_ENV_VARS'] = original
   end
 
-  before(:each) do
-    allow_command(params['command']).always_return(output)
-    allow_script(params['script']).always_return(output)
-  end
-
   it 'errors when passing command and script' do
-    result = run_plan('secure_env_vars', params)
+    result = run_plan('secure_env_vars', command_params.merge({ 'script' => 'foobar' }))
     expect(result.ok?).not_to be
     expect(result.value.msg).to match(/Cannot specify both script and command for secure_env_vars/)
   end
 
   it 'errors when passing neither command nor script' do
-    result = run_plan('secure_env_vars', 'targets' => 'localhost')
+    result = run_plan('secure_env_vars', target)
     expect(result.ok?).not_to be
     expect(result.value.msg).to match(/Must specify either script or command for secure_env_vars/)
   end
 
   it 'succeeds with a command' do
-    expect_command(params['command']).with_targets([params['targets']]).with_params('_env_vars' => env_vars)
+    expect_command(command).with_targets([target['targets']])
+      .with_params('_env_vars' => env_vars)
     result = run_plan('secure_env_vars', command_params)
     expect(result.ok?).to be
   end
 
   it 'succeeds with a script' do
-    expect_script(params['script']).with_targets([params['targets']]).with_params('_env_vars' => env_vars)
-    result = run_plan('secure_env_vars', script_params)
-    expect(result.ok?).to be
+    Tempfile.open(%w[script rb], File.join(Dir.pwd, 'spec', 'fixtures', 'modules', 'secure_env_vars', 'files')) do |file|
+      file.binmode # Stop Ruby implicitly doing CRLF translations and breaking tests
+      file.write("puts Hello")
+      file.flush
+
+      puppet_path = File.join('secure_env_vars', Pathname.new(file.path).basename)
+      expect_script(puppet_path).with_targets([target['targets']])
+        .with_params('_env_vars' => env_vars)
+      result = run_plan('secure_env_vars', target.merge({ 'script' => file.path }))
+      expect(result.ok?).to be
+    end
   end
 
   it 'passes an empty hash to env_vars when BOLT_ENV_VARS is not set' do
     ENV.delete('BOLT_ENV_VARS')
-    expect_command(params['command']).with_targets([params['targets']]).with_params('_env_vars' => {})
+    expect_command(command).with_targets([target['targets']])
+      .with_params('_env_vars' => {})
     result = run_plan('secure_env_vars', command_params)
     expect(result.ok?).to be
   end
